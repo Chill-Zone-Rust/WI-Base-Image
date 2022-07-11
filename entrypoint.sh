37e5ae7d254b634d63d73117d28e4b24b7d57bc5
Hayden Andreyka <haydenandreyka@gmail.com>
2022-07-11 15:48:34 -0700

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,9 +1,6 @@
 #!/bin/bash
 cd /home/container
 
-# Make internal Docker IP address available to processes.
-export INTERNAL_IP=`ip route get 1 | awk '{print $(NF-2);exit}'`
-
 # Create template file if it doesn't exist
 FILE=server.conf
 if [ ! -f "$FILE" ]; then
@@ -11,7 +8,7 @@ if [ ! -f "$FILE" ]; then
 fi
 
 # Copy nginx config file to nginx folder
-cp server.conf /etc/nginx/templates/server.template
+cp server.conf /etc/nginx/conf.d/default.conf
 
 # Run the Server
 ${STARTUP}
